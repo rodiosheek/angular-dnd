1fcb43b209b8e1897f03d92dfd5ed44263e2dc5e
rodiontopagent <rodion@top-agt.com>
2018-02-08 16:00:43 +0200

    fixed asserts pathes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'build/public')))
+  .use(express.static(path.join(__dirname, 'build/')))
 
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'build/index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
